f0c879f2d3e36d1179ad7a25c9eff6d7334609c6
ktp <t08265hk@gmail.com>
2019-09-01 20:09:17 +0900

    [add] sign in form.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,31 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Input, Button } from "semantic-ui-react";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Sign in to frontend-validation</p>
+        <p>Sign in</p>
+        <section>
+          <Input
+            type="mail"
+            className="App-input"
+            placeholder="Email"
+            autoComplete="off"
+          />
+          <Input
+            type="password"
+            className="App-input"
+            placeholder="Password"
+            autoComplete="off"
+          />
+        </section>
+        <Button className="App-button" primary>
+          Sign in
+        </Button>
         <a
           className="App-link"
           href="https://github.com/kato1628/frontend-validation"
