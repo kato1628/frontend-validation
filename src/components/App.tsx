00602760159aac8f78b770600acbd93fac9cc391
ktp <t08265hk@gmail.com>
2019-09-01 19:30:21 +0900

    [change] title and link.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,16 +7,14 @@ const App: React.FC = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/components/App.tsx</code> and save to reload.
-        </p>
+        <p>Sign in to frontend-validation</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/kato1628/frontend-validation"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn more
         </a>
       </header>
     </div>
